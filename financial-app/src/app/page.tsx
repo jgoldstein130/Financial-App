f3814e6a06c056093a329c2d9ccb4705454a56b2
Josh Goldstein <jgoldstein130@gmail.com>
2025-04-27 19:32:53 -0500

    install material ui

--- financial-app/src/app/page.tsx
+++ financial-app/src/app/page.tsx
@@ -1,5 +1,27 @@
+'use client'
+
+// TODO: make this a server component but make a new component for the accordion which is a client component
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+
 const Home = () => {
-  return <div>financial app</div>;
+  return <div>financial app
+    <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion 1
+        </AccordionSummary>
+        <AccordionDetails>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+  </div>;
 };
 
 export default Home;
