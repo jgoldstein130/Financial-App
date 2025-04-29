85522681ab53f537bc4941172caa53a53bd034c6
Josh Goldstein <jgoldstein130@gmail.com>
2025-04-28 19:21:26 -0500

    able to update fields of accounts

--- financial-app/src/app/Components/AccountAccordion.tsx
+++ financial-app/src/app/Components/AccountAccordion.tsx
@@ -3,10 +3,42 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import TextField from "@mui/material/TextField";
-import { ReactNode } from "react";
+import { ChangeEvent, ReactNode } from "react";
 import Box from "@mui/material/Box";
+import { Account } from "../page";
 
 const AccountAccordion = ({ children, ...props }: Props) => {
+  const handleAccountValueChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: string
+  ) => {
+    if (!props.account.name) return;
+
+    if (field === "startingBalance") {
+      const sanitizedValue = e.target.value.replace(/[^0-9.]/g, "");
+      const formattedValue = sanitizedValue ? `$${sanitizedValue}` : "";
+
+      props.updateAccount(props.account.name, {
+        ...props.account,
+        startingBalance: formattedValue,
+      });
+    }
+  };
+
+  const cleanAccountValue = (field: string) => {
+    if (!props.account.name) return;
+
+    if (field === "startingBalance") {
+      const newStartingBalance = props.account.startingBalance
+        ? "$" + Number(props.account.startingBalance.slice(1)).toFixed(2)
+        : "";
+      props.updateAccount(props.account.name, {
+        ...props.account,
+        startingBalance: newStartingBalance,
+      });
+    }
+  };
+
   return (
     <Accordion>
       <AccordionSummary
@@ -14,14 +46,16 @@ const AccountAccordion = ({ children, ...props }: Props) => {
         aria-controls="panel1-content"
         id="panel1-header"
       >
-        {props.name}
+        {props.account.name}
       </AccordionSummary>
       <AccordionDetails>
         <Box display="flex" flexDirection="column" gap={2}>
           <TextField
             label="Starting Balance"
             variant="outlined"
-            type="number"
+            value={props.account.startingBalance || ""}
+            onChange={(e) => handleAccountValueChange(e, "startingBalance")}
+            onBlur={() => cleanAccountValue("startingBalance")}
           />
           <TextField
             label="Annual Interest (%)"
@@ -42,7 +76,8 @@ const AccountAccordion = ({ children, ...props }: Props) => {
 
 interface Props {
   children?: ReactNode;
-  name: string;
+  account: Account;
+  updateAccount: (name: string, updatedAccount: Account) => void;
 }
 
 export default AccountAccordion;
--- financial-app/src/app/page.tsx
+++ financial-app/src/app/page.tsx
@@ -5,15 +5,31 @@ import CardContent from "@mui/material/CardContent";
 import AccountAccordion from "./Components/AccountAccordion";
 import { useState } from "react";
 
-interface Account {
-  name: string;
+export interface Account {
+  name?: string;
+  startingBalance?: string;
+  annualInterest?: string;
+  monthlyContribution?: string;
 }
+
+// TODO: add check to make sure two accounts aren't named the same
 
 const Home = () => {
   const [accounts, setAccounts] = useState<Account[]>([]);
 
   const addAccount = () => {
-    setAccounts((prevAccounts) => [...prevAccounts, { name: "Roth IRA" }]);
+    setAccounts((prevAccounts) => [
+      ...prevAccounts,
+      { name: "Account " + (prevAccounts.length + 1) },
+    ]);
+  };
+
+  const updateAccountByName = (name: string, updatedAccount: Account) => {
+    setAccounts((prevAccounts) =>
+      prevAccounts.map((account) =>
+        account.name === name ? updatedAccount : account
+      )
+    );
   };
 
   return (
@@ -28,7 +44,8 @@ const Home = () => {
           </button>
           {accounts.map((account, index) => (
             <AccountAccordion
-              name={account.name}
+              account={account}
+              updateAccount={updateAccountByName}
               key={account.name + "-" + index}
             />
           ))}
