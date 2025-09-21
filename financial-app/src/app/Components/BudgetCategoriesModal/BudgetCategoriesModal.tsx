3632d518090a5e430326861cea8bd16d9e51b5e9
Josh Goldstein <jgoldstein130@gmail.com>
2025-09-21 15:21:16 -0500

    Finish Categories Functionality

--- financial-app/src/app/Components/BudgetCategoriesModal/BudgetCategoriesModal.tsx
+++ financial-app/src/app/Components/BudgetCategoriesModal/BudgetCategoriesModal.tsx
@@ -10,68 +10,65 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from "@mui/material";
 import { SketchPicker } from "react-color";
 import { ColorResult } from "react-color";
+import { Category } from "../BudgetSection/BudgetSection";
 
 const BudgetCategoriesModal = ({ children, ...props }: Props) => {
   const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
-  const [categoryNameForColorPicker, setCategoryNameForColorPicker] = useState<string>("");
+  const [categoryForColorPicker, setCategoryForColorPicker] = useState<string>("");
   const [selectedColor, setSelectedColor] = useState<string>("white");
 
-  const handleColorPickerChange = (categoryName: string, color: string) => {
-    // need a new function for editing the color on a category.
-  };
-
-  // and we will need one for editing the name on the category.  maybe the categories map should map a uuid to a [categoryName, color] instead.
   return (
     <>
       <Modal open={props.isOpen} onClose={props.onClose}>
+        {/* TODO: this box should be its own component and each modal should use it*/}
         <Box
           sx={{
             position: "absolute",
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 600,
-            minHeight: 400,
+            width: 500,
+            height: 500,
             bgcolor: "background.paper",
             boxShadow: 24,
             p: 4,
+            borderRadius: "5px",
           }}
         >
-          Categories
-          <TableContainer component={Paper} sx={{ width: "100%" }}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Category</TableCell>
-                  <TableCell align="left"></TableCell>
-                  <TableCell align="right"></TableCell>
-                </TableRow>
-              </TableHead>
+          <div className="mb-4 font-bold">Categories</div>
+          <TableContainer component={Paper} style={{ maxHeight: 350, overflowY: "auto" }}>
+            <Table stickyHeader aria-label="category table">
               <TableBody>
-                {[...props.categories.entries()].map(([category, color], index) => (
-                  <TableRow key={category + "-" + index}>
-                    <TableCell component="th" scope="row">
-                      {category}
+                {[...props.categories.entries()].map(([categoryKey, category], index) => (
+                  <TableRow key={categoryKey}>
+                    <TableCell component="th" scope="row" className="w-full">
+                      <div className="flex gap-2 items-center">
+                        <div
+                          style={{
+                            width: "25px",
+                            height: "25px",
+                            backgroundColor: category.color,
+                            borderRadius: "5px",
+                            border: "1px solid black",
+                          }}
+                          onClick={() => {
+                            setShowColorPicker(true);
+                            setCategoryForColorPicker(categoryKey);
+                          }}
+                        />
+                        <TextField
+                          value={category.categoryName}
+                          onChange={(e) => props.updateCategoryName(categoryKey, e.target.value)}
+                          onBlur={() => props.makeCategoryNameUnique(categoryKey)}
+                          variant="standard"
+                        ></TextField>
+                      </div>
                     </TableCell>
-                    <TableCell align="left">
-                      <div
-                        style={{
-                          width: "25px",
-                          height: "25px",
-                          backgroundColor: color,
-                          borderRadius: "5px",
-                          border: "1px solid black",
-                        }}
-                        onClick={() => {
-                          setShowColorPicker(true);
-                          setCategoryNameForColorPicker(category);
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell align="right" onClick={() => props.removeCategory(category)}>
+                    <TableCell align="right" onClick={() => props.removeCategory(categoryKey)}>
                       Delete
                     </TableCell>
                   </TableRow>
@@ -102,7 +99,7 @@ const BudgetCategoriesModal = ({ children, ...props }: Props) => {
               <SketchPicker
                 color={selectedColor}
                 onChange={(color: ColorResult) => {
-                  props.updateCategoryColor(categoryNameForColorPicker, color.hex);
+                  props.updateCategoryColor(categoryForColorPicker, color.hex);
                   setSelectedColor(color.hex);
                 }}
               />
@@ -118,10 +115,12 @@ interface Props {
   children?: ReactNode;
   isOpen: boolean;
   onClose: () => void;
-  categories: Map<string, string>;
-  addCategory: (category: string, color: string) => void;
-  removeCategory: (category: string) => void;
-  updateCategoryColor: (category: string, color: string) => void;
+  categories: Map<string, Category>;
+  addCategory: (categoryName: string, color: string) => void;
+  removeCategory: (categoryKey: string) => void;
+  updateCategoryColor: (categoryKey: string, color: string) => void;
+  updateCategoryName: (categoryKey: string, categoryName: string) => void;
+  makeCategoryNameUnique: (categoryKey: string) => void;
 }
 
 export default BudgetCategoriesModal;
